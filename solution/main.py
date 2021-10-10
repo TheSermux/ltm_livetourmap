baf48c183248b8f9a1f981076f35c66d17e2994b
babaAH <shama_chich@icloud.com>
2021-10-10 16:15:00 +0300

    work front on nginx service

--- solution/main.py
+++ solution/main.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
+#import os
+
 import tour_score_parser as tsp
 import analyzed_messages as am
 
 # вызов функции, которая возвращает json
 result = tsp.get_json_with_parsed_reviews_info(am.reviews_to_analyze)
 print(result)
+#application = get_wsgi_application()
+
